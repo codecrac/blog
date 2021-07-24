78feebae5123547e16e2a74a67d0854be8bf633f
yves santoz <yvessantoz@gmail.com>
2021-07-24 00:35:23 +0000

    deplacer emplacement evenement sur l'accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,57 +3,6 @@
 @section('body')
 
     <div class="main-content innerpagebg wf100">
-        <!--News Large Page Start-->
-
-{{--====================EVENEMENTS ========================--}}
-
-        @if(sizeof($les_evenements)>0)
-
-            <div class="section-title mt-4">
-                <h2 > <span class="table-bordered p-3"> Evenements à Venir </span> </h2>
-            </div>
-        <div class="container">
-            <div class="row">
-                @foreach($les_evenements as $item)
-                <div class="col-lg-4 col-md-6">
-                    <div class="ng-box">
-                        <div class="thumb">
-                            <a href="{{route('details_evenement',[$item['id']])}}">
-
-                                <img src="data:image/jpeg;base64,{{$item['image']}}" style="max-height: 280px;" alt="">
-
-                                <h6 style="text-align: center;position: absolute;bottom:0;width:100%;padding:10px;color: #fff;background-color:rgba(0,0,0,0.7)">
-                                    {{$item['titre']}}
-                                </h6>
-                            </a>
-                        </div>
-                        <div class="ng-txt">
-                            <div class="post-meta row">
-                             {{--   <div class="col-md-4">
-                                    <b> {{date('d-m-Y',strtotime($item['date_evenement']))}}</b>
-                                </div>--}}
-                                <div class="col-md-12 text-center">
-                                    <a target="_blank"
-                                       alt="{{date('Y-m-d',strtotime($item['date_evenement']))}}"
-                                       style="font-weight: bold;color:#222" href="https://logwork.com/countdown-qqqv" class="countdown-timer"
-                                       onclick="return false;" data-timezone="Africa/Lagos" data-language="fr" data-date="{{date('Y-m-d',strtotime($item['date_evenement']))}} 00:00">Bientot</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-            </div>
-        </div>
-
-
-        <br/>
-            <div class="section-title">
-                <h2 > <span class="table-bordered p-3 text-capitalize">à la une </span> </h2>
-            </div>
-        @endif
-
-{{--====================FIN EVENEMENTS ========================--}}
 
         <!--Start-->
         <div class="news-large">
@@ -168,6 +117,51 @@
                     @endforeach--}}
                 </div>
 
+
+
+{{--====================EVENEMENTS ========================--}}
+
+            @if(sizeof($les_evenements)>0)
+
+                <div class="section-title mt-4">
+                    <h2 > <span class="table-bordered p-3"> Evenements à Venir </span> </h2>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        @foreach($les_evenements as $item)
+                            <div class="col-lg-4 col-md-6">
+                                <div class="ng-box">
+                                    <div class="thumb">
+                                        <a href="{{route('details_evenement',[$item['id']])}}">
+
+                                            <img src="data:image/jpeg;base64,{{$item['image']}}" style="max-height: 280px;" alt="">
+
+                                            <h6 style="text-align: center;position: absolute;bottom:0;width:100%;padding:10px;color: #fff;background-color:rgba(0,0,0,0.7)">
+                                                {{$item['titre']}}
+                                            </h6>
+                                        </a>
+                                    </div>
+                                    <div class="ng-txt">
+                                        <div class="post-meta row">
+                                            {{--   <div class="col-md-4">
+                                                   <b> {{date('d-m-Y',strtotime($item['date_evenement']))}}</b>
+                                               </div>--}}
+                                            <div class="col-md-12 text-center">
+                                                <a target="_blank"
+                                                   alt="{{date('Y-m-d',strtotime($item['date_evenement']))}}"
+                                                   style="font-weight: bold;color:#222" href="https://logwork.com/countdown-qqqv" class="countdown-timer"
+                                                   onclick="return false;" data-timezone="Africa/Lagos" data-language="fr" data-date="{{date('Y-m-d',strtotime($item['date_evenement']))}} 00:00">Bientot</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            @endif
+
+{{--====================FIN EVENEMENTS ========================--}}
 {{--====================Articles Classé par categories ========================--}}
 
             @foreach($menu_present_sur_accueil as $item_categorie_simple)
